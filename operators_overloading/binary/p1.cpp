9d66f2e9eb2a6e4f1d6a4d470d02e8684d6db4fc
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-04-25 15:32:03 +0530

    added

--- operators_overloading/binary/p1.cpp
+++ operators_overloading/binary/p1.cpp
@@ -3,20 +3,24 @@ using namespace std;
 class xyz{
            int a;
            public:
-               xyz(){
+               void enter(){
                    cout<<"Enter A:-";
                    cin>>a;
                }
-               void operator ++(){
-                 ++a;
+               xyz operator +(xyz x1){
+                  xyz x2;
+                  x2.a = a+x1.a;
+                  return x2;
                }
                void disp(){
-                cout<<"a is:-"<<a;
+                  cout<<"a is :- "<<a;
                }
 };
 int main(){
-            xyz x1;
-            ++x1;
-            x1.disp();
+            xyz x1,x2,x3;
+            x1.enter();
+            x2.enter();
+            x3 = x1+x2;
+            x3.disp();
             return 0;
 }
