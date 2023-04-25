7c01e45b6bc580753140229fb24375301e65f339
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-04-25 14:45:09 +0530

    added file

---
+++ polymorphisam/overload/p1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+#define PI 3.14
+float area(float r){
+      return PI*r*r;
+}
+float area(float l,float h){
+      return l*h;
+}
+float area(float l,float h,float b){
+    return l*h*b;
+}
+int main(){
+            int no;
+            cout<<"Enter number:-";
+            cin>>no;
+            cout<<"area of circle is:-"<<area(no)<<endl;
+            cout<<"area of Rectangle is:-"<<area(no,no,no)<<endl;
+            cout<<"area of tringle:-"<<area(no,no)<<endl;
+            return 0;
+}
