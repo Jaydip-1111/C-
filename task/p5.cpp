9e3821f82e82c5267b0cbc1a880f73ac588ad913
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-05-01 20:26:58 +0530

    added

---
+++ task/p5.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+int main()
+{
+       char str[100];
+       int i;
+       cout<<"Enter Any String:-";
+       cin>>str;
+       i = 0;
+       while(str[i] !='\0')
+       {
+           if(str[i] >= 'a' && str[i] <= 'z')
+           {
+               str[i] = str[i] -32;
+           }
+           else if(str[i] >='A' && str[i] <='Z')
+           {
+              str[i] = str[i] + 32;
+           }
+              i++;
+       }
+        cout << "convert string to toggle= " << str;
+        return 0;
+}
